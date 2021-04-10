d45b3139dc9e63ee7cca18b60d0cd3fca4c41d5b
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-10 14:28:16 +1000

    Added Router and Material UI Theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+// Routing Stuff
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Authentication Context Provider
+import { AuthProvider } from "./context/auth";
+
+// Pages
+import Home from "./pages/Home";
+
+// Material UI Theme Function
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+// Styling
 import "./App.css";
+
+// Theme
+const theme = createMuiTheme({
+   palette: {
+      type: "dark",
+   },
+});
 
 function App() {
    return (
-      <div className="App">
-         <h1>Hello There</h1>
-      </div>
+      <AuthProvider>
+         <Router>
+            <Switch>
+               <ThemeProvider theme={theme}>
+                  <CssBaseline />
+                  <Route exact path="/" component={Home} />
+               </ThemeProvider>
+            </Switch>
+         </Router>
+      </AuthProvider>
    );
 }
 

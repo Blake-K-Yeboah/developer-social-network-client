6568059287e6317e13ae30cdbfeaa911d3420535
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-11 22:27:06 +1000

    Added Register Form to Register Page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,10 @@ import { Helmet } from "react-helmet";
 
 // Page Components
 import Navbar from "../components/Navbar";
+import RegisterForm from "../components/RegisterForm";
+
+// Material UI Components
+import { Container, Typography } from "@material-ui/core";
 
 const Register = () => {
    return (
@@ -11,6 +15,22 @@ const Register = () => {
             <title>Developer Social Network - Register</title>
          </Helmet>
          <Navbar />
+         <br />
+         <br />
+         <br />
+         <br />
+         <Container>
+            <Typography variant="h3" component="h3">
+               Register
+            </Typography>
+            <br />
+            <Typography variant="body1" component="p">
+               Fill out the following form to register
+            </Typography>
+            <br />
+            <br />
+            <RegisterForm />
+         </Container>
       </>
    );
 };
